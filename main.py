0e61e48cf2436d8a9c2fdf1f6cde5129c28b2b39
marcin <s12497@pjwstk.edu.pl>
2023-10-10 18:01:20 +0200

    object board

--- main.py
+++ main.py
@@ -1,55 +1,64 @@
-def initialize_board(size):
-    board = [['.' for _ in range(size)] for _ in range(size)]
-    return board
+class Board:
+    def __init__(self, size):
+        self.size = size
+        self.board = [['.' for _ in range(size)] for _ in range(size)]
+        self.current_player = 'x'
 
-def print_board(board):
-    print("  " + " ".join(str(i) for i in range(1, len(board) + 1)))
-    for i in range(len(board)):
-        print(f"{i+1} {' '.join(board[i])}")
+    def print(self):
+        print("  " + " ".join(str(i) for i in range(1, len(self.board) + 1)))
+        for i in range(len(self.board)):
+            print(f"{i + 1} {' '.join(self.board[i])}")
 
-def check_win(board, row, col, player):
-    # Check for a winning condition
-    directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
-    for dr, dc in directions:
-        count = 1
-        r, c = row, col
-        while 0 <= r + dr < len(board) and 0 <= c + dc < len(board) and board[r+dr][c+dc] == player:
-            count += 1
-            r, c = r + dr, c + dc
-            if count == 5:
-                return True
-    return False
+    def make_move(self, row, col):
+        if self.board[row][col] == '.':
+            self.board[row][col] = self.current_player
+            return True
+        else:
+            return False
 
-def is_draw(board):
-    return all(cell != '.' for row in board for cell in row)
+    def check_win(self, row, col):
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        for dr, dc in directions:
+            count = 1
+            r, c = row, col
+            while 0 <= r + dr < self.size and 0 <= c + dc < self.size and self.board[r + dr][c + dc] == self.current_player:
+                count += 1
+                r, c = r + dr, c + dc
+                if count == 5:
+                    return True
+        return False
+
+    def is_draw(self):
+        return all(cell != '.' for row in self.board for cell in row)
+
+    def switch_player(self):
+        self.current_player = 'o' if self.current_player == 'x' else 'x'
+
 
 def main():
     size = 9
-    board = initialize_board(size)
-    player = 'x'
+    board = Board(size)
 
     while True:
-        print_board(board)
-        row = int(input(f'Player {player}, enter row (1-{size}): ')) - 1
-        col = int(input(f'Player {player}, enter col (1-{size}): ')) - 1
+        board.print()
+        row = int(input(f'Player {board.current_player}, enter row (1-{size}): ')) - 1
+        col = int(input(f'Player {board.current_player}, enter col (1-{size}): ')) - 1
 
-        if board[row][col] == '.':
-            board[row][col] = player
-
-            if check_win(board, row, col, player):
-                print_board(board)
-                print(f'Player {player} wins!')
+        if board.make_move(row, col):
+            if board.check_win(row, col):
+                board.print()
+                print(f'Player {board.current_player} wins!')
                 break
 
-            if is_draw(board):
-                print_board(board)
+            if board.is_draw():
+                board.print()
                 print('It\'s a draw!')
                 break
 
-            player = 'o' if player == 'x' else 'x'
+            board.switch_player()
         else:
             print('Invalid move. Try again.')
 
+
 if __name__ == "__main__":
     main()
-
