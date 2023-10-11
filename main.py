11ae29f10def85406b776380011f0f136ebc394c
marcin <s12497@pjwstk.edu.pl>
2023-10-11 18:14:37 +0200

    initial AI version

--- main.py
+++ main.py
@@ -1,64 +1,87 @@
-class Board:
+from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
+
+
+class Board(TwoPlayerGame):
     def __init__(self, size):
         self.size = size
         self.board = [['.' for _ in range(size)] for _ in range(size)]
         self.current_player = 'x'
 
-    def print(self):
+    def show(self):
         print("  " + " ".join(str(i) for i in range(1, len(self.board) + 1)))
         for i in range(len(self.board)):
             print(f"{i + 1} {' '.join(self.board[i])}")
 
-    def make_move(self, row, col):
+    def make_move(self, coord):
+        row = coord(0)
+        col = coord(1)
         if self.board[row][col] == '.':
             self.board[row][col] = self.current_player
             return True
         else:
             return False
 
-    def check_win(self, row, col):
+    def possible_moves(self):
+        moves = []
+        for row in range(self.size):
+            for col in range(self.size):
+                if self.board[row][col] == '.':
+                    moves.append((row, col))
+        return moves
+
+    def win(self):
         directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
-        for dr, dc in directions:
-            count = 1
-            r, c = row, col
-            while 0 <= r + dr < self.size and 0 <= c + dc < self.size and self.board[r + dr][c + dc] == self.current_player:
-                count += 1
-                r, c = r + dr, c + dc
-                if count == 5:
-                    return True
+
+        def check_direction(start_row, start_col, dr, dc, symbol):
+            for i in range(5):
+                r, c = start_row + i * dr, start_col + i * dc
+                if not (0 <= r < len(self.board) and 0 <= c < len(self.board[0]) and self.board[r][c] == symbol):
+                    return False
+            return True
+
+        for start_row in range(len(self.board)):
+            for start_col in range(len(self.board[0])):
+                symbol = self.board[start_row][start_col]
+                if symbol in ['x', 'o']:
+                    for dr, dc in directions:
+                        if check_direction(start_row, start_col, dr, dc, symbol):
+                            return True
         return False
 
-    def is_draw(self):
-        return all(cell != '.' for row in self.board for cell in row)
+    def is_over(self):
+        return self.win() or all(cell != '.' for row in self.board for cell in row)
 
     def switch_player(self):
         self.current_player = 'o' if self.current_player == 'x' else 'x'
 
-
-def main():
-    size = 9
-    board = Board(size)
-
-    while True:
-        board.print()
-        row = int(input(f'Player {board.current_player}, enter row (1-{size}): ')) - 1
-        col = int(input(f'Player {board.current_player}, enter col (1-{size}): ')) - 1
-
-        if board.make_move(row, col):
-            if board.check_win(row, col):
-                board.print()
-                print(f'Player {board.current_player} wins!')
-                break
-
-            if board.is_draw():
-                board.print()
-                print('It\'s a draw!')
-                break
-
-            board.switch_player()
-        else:
-            print('Invalid move. Try again.')
+    def scoring(self):
+        return 100 if self.win() else 0
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     size = 9
+#     board = Board(size)
+#
+#     while True:
+#         board.show()
+#         moves = board.possible_moves()
+#         print(f'Possible moves: {moves}')
+#
+#         row = int(input(f'Player {board.current_player}, enter row (1-{size}): ')) - 1
+#         col = int(input(f'Player {board.current_player}, enter col (1-{size}): ')) - 1
+#
+#         if (row, col) in moves:
+#             if board.make_move(row, col):
+#                 if board.win():
+#                     board.show()
+#                     print(f'Player {board.current_player} wins!')
+#                     break
+#
+#                 board.switch_player()
+#         else:
+#             print('Invalid move. Try again.')
+
+
+ai = Negamax(10)  # The AI will think 10 moves in advance
+game = Board([AI_Player(ai), Human_Player()])
+history = game.play()
